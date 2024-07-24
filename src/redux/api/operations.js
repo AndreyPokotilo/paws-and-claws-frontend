1aa1ac4b41f8c652333402548804f1ff6dff1290
AndreyPokotilo <dpon444gmail.com>
2024-07-24 21:23:06 +0200

    fixed URI

--- src/redux/api/operations.js
+++ src/redux/api/operations.js
@@ -4,9 +4,9 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const api = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
+    baseUrl: "https://paws-and-claws-backend.onrender.com",
     // baseUrl: 'https://paws-and-claws-store.onrender.com',
     // baseUrl: "https://paws-and-claws-store-k6lb.onrender.com",
-    baseURL: "https://paws-and-claws-backend.onrender.com",
     // baseUrl: 'http://localhost:4000',
   }),
   //   tagTypes: ['Products'],
