4f6d25159895f1839993d267ea626ecca164ece8
AndreyPokotilo <dpon444gmail.com>
2024-07-31 20:40:24 +0200

    fixed code

--- src/services/auth.js
+++ src/services/auth.js
@@ -9,79 +9,98 @@ const instance = axios.create({
 });
 
 const setToken = token => {
-    return (instance.defaults.headers.common.authorization = token?  `Bearer ${token}` : '');
+  return (instance.defaults.headers.common.authorization = token
+    ? `Bearer ${token}`
+    : '');
 };
-
 
 instance.interceptors.response.use(
   response => response,
   async error => {
-       
-    if (error?.response?.status === 401 && !error?.config?._retry && error.request.responseURL !== 'https://paws-and-claws-backend/api/auth/verifyResetToken') {
-        const { data } = await instance.post('/api/auth/refresh', {}, {withCredentials: true});
-        localStorage.setItem('accessToken',data?.accessToken);
+    if (
+      error?.response?.status === 401 &&
+      !error?.config?._retry &&
+      error.request.responseURL !==
+        'https://paws-and-claws-backend/api/auth/verifyResetToken'
+    ) {
+      try {
+        const { data } = await instance.post(
+          '/api/auth/refresh',
+          {},
+          { withCredentials: true },
+        );
+        localStorage.setItem('accessToken', data?.accessToken);
         error.config.headers.authorization = `Bearer ${data?.accessToken}`;
+        error.config._retry = true;
         setToken(data?.accessToken);
         return instance(error.config);
-  }
+      } catch (refeshError) {
+        console.error('Unable to refresh token', refeshError);
+        Promise.reject(refeshError);
+      }
+    }
+
     return Promise.reject(error);
   },
 );
 
-export const register = async (newUser) => {
-    const { data: result } = await instance.post("/api/auth/register", newUser);    
-    setToken(result.data.user.accessToken);
-    localStorage.setItem("accessToken", result.data.user.accessToken);
-    return result;
+export const register = async newUser => {
+  const { data: result } = await instance.post('/api/auth/register', newUser);
+  setToken(result.data.user.accessToken);
+  localStorage.setItem('accessToken', result.data.user.accessToken);
+  return result;
 };
 
-export const login = async (user) => {
-    const { data: result } = await instance.post("/api/auth/login", user);
-    setToken(result.data.user.accessToken);
-    localStorage.setItem("accessToken", result.data.user.accessToken);
-    return result;
+export const login = async user => {
+  const { data: result } = await instance.post('/api/auth/login', user);
+  setToken(result.data.user.accessToken);
+  localStorage.setItem('accessToken', result.data.user.accessToken);
+  return result;
 };
 
 export const logout = async () => {
-    const { data } = await instance.get("/api/auth/logout");
-    setToken();
-    localStorage.clear()
-    return data;
-};
-
-export const getCurrent = async () => {
-    try {
-        const accessToken = localStorage.getItem('accessToken');
-        setToken(accessToken);
-
-        const { data } = await instance.get("/api/auth/current", {withCredentials: true});
-        return data;
-    }
-    catch (error) {
-        console.log("error:", error)
-        setToken();
-        throw error;
-    }
-};
-
-export const resendVerifyEmail = async (email) => {
-    const data = await instance.post('/api/auth/verify', {email});
-    return data;
-};
-
-export const resetPassword = async (email) => {
-  const data = await instance.post('/api/auth/resetPassword', {email});
+  const { data } = await instance.get('/api/auth/logout');
+  setToken();
+  localStorage.clear();
   return data;
 };
 
-export const verifyResetToken = async (resetPasswordToken) => {
-  const data = await instance.post('/api/auth/verifyResetToken', {resetPasswordToken});
+export const getCurrent = async () => {
+  try {
+    const accessToken = localStorage.getItem('accessToken');
+    setToken(accessToken);
+
+    const { data } = await instance.get('/api/auth/current', {
+      withCredentials: true,
+    });
+    return data;
+  } catch (error) {
+    console.log('error:', error);
+    setToken();
+    throw error;
+  }
+};
+
+export const resendVerifyEmail = async email => {
+  const data = await instance.post('/api/auth/verify', { email });
+  return data;
+};
+
+export const resetPassword = async email => {
+  const data = await instance.post('/api/auth/resetPassword', { email });
+  return data;
+};
+
+export const verifyResetToken = async resetPasswordToken => {
+  const data = await instance.post('/api/auth/verifyResetToken', {
+    resetPasswordToken,
+  });
   return data.status;
 };
 
-export const updatePassword = async (newPass) => {
+export const updatePassword = async newPass => {
   const data = await instance.patch('/api/auth/updatePassword', newPass);
-  return data.request.status
+  return data.request.status;
 };
 
 export default instance;
