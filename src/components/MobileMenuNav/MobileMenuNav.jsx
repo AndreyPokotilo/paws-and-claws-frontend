b1191718c492ebe68a2b95ce673b1678d556b2f2
AndreyPokotilo <dpon444gmail.com>
2024-08-25 21:45:05 +0200

    cheanged mob menu

--- src/components/MobileMenuNav/MobileMenuNav.jsx
+++ src/components/MobileMenuNav/MobileMenuNav.jsx
@@ -1,17 +1,51 @@
-import { Link, NavStyle } from "components";
-import { MenuContainer } from "./MobileMenuNav.styled";
+import { Leng, LengLinkStyled, Link, NavStyle } from 'components';
+import { MenuContainer } from './MobileMenuNav.styled';
 
-
-export const MobailMenuNav = ({setMobileMenuTogle}) => {
-    return(
+export const MobailMenuNav = ({ setMobileMenuTogle }) => {
+  return (
     <MenuContainer>
-        <NavStyle>
-              <Link onClick={()=>setMobileMenuTogle(false)} to={'catalog'}>КАТАЛОГ</Link>
-              <Link onClick={()=>setMobileMenuTogle(false)} to={'prices-drop'}>АКЦІЇ</Link>
-              <Link onClick={()=>setMobileMenuTogle(false)} to={'brands'}>БРЕНДИ</Link>
-              <Link onClick={()=>setMobileMenuTogle(false)} to={'aboutUs'}>ПРО КОМПАНІЮ</Link>
-              <Link onClick={()=>setMobileMenuTogle(false)} to={'contacts'}>MOBA</Link>
-            </NavStyle>
+      <NavStyle>
+        <div>
+          <p>КАТАЛОГ</p>
+          <Link onClick={() => setMobileMenuTogle(false)} to={'catalog'}>
+          КАТАЛОГ
+        </Link>
+        </div>
+        <div>
+          <Link onClick={() => setMobileMenuTogle(false)} to={'prices-drop'}>
+          АКЦІЇ
+        </Link>
+        </div>
+        <div>
+          <Link onClick={() => setMobileMenuTogle(false)} to={'brands'}>
+          БРЕНДИ
+        </Link>
+        </div>
+        <div>
+          <p>ПРО КОМПАНІЮ</p>
+          <Link onClick={() => setMobileMenuTogle(false)} to={'aboutUs'}>
+          Про компанію
+        </Link>
+        <Link onClick={() => setMobileMenuTogle(false)} to={'contacts'}>
+        Контакти
+        </Link>
+        </div>
+        <div>
+          <p>MOBA</p>
+          <Link onClick={() => setMobileMenuTogle(false)} to={'contacts'}>
+          MOBA
+        </Link>
+        <Leng>
+              <LengLinkStyled>Eng</LengLinkStyled>
+              <LengLinkStyled className="accent">Укр</LengLinkStyled>
+            </Leng>
+        </div>
+        
+        
+        
+        
+        
+      </NavStyle>
     </MenuContainer>
-    );
-}
+  );
+};
